dd635a2e249f8c527e8229c178f50ffce481e83b
mauriciomaniglia <manigliamauricio@gmail.com>
2022-07-03 16:46:47 -0300

    Create initial project Sandwiches to test SwiftUI framework

---
+++ Sandwiches/Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Mauricio Maniglia on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
