2dd4913485982be68cc3ab800cdd4f5c62835171
mauriciomaniglia <manigliamauricio@gmail.com>
2022-07-06 20:54:04 -0300

    Initial project setup for Landkmarks

---
+++ Landmarks/Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Mauricio Maniglia on 06/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
