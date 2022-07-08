6b52a61664fc706bb80b08bd72ec1e43ac5f5afb
mauriciomaniglia <manigliamauricio@gmail.com>
2022-07-08 11:18:37 -0300

    Create list for landmarks.

---
+++ Landmarks/Shared/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Mauricio Maniglia on 08/07/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
