a16b9b32f7921825c9ff7ae05c09a9745454c587
mauriciomaniglia <manigliamauricio@gmail.com>
2022-07-16 21:55:07 -0300

    Add a Category View

---
+++ Landmarks/Shared/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Mauricio Maniglia on 16/07/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
