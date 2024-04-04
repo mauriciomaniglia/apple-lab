ebbf4633ceb31cb0dd70ff743af36859292bd62f
mauriciomaniglia <manigliamauricio@gmail.com>
2024-04-04 16:09:18 -0300

    Add swipe card animation

--- TinderClone/TinderClone/Core/Cards/Views/CardView.swift
+++ TinderClone/TinderClone/Core/Cards/Views/CardView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CardView: View {
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: Double = 0
+
     var body: some View {
         ZStack(alignment: .bottom) {
             Image(.meganfox1)
@@ -19,10 +22,38 @@ struct CardView: View {
         }
         .frame(width: cardWidth, height: cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy, value: xOffset)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged)
+                .onEnded(onDragEnded)
+        )
+    }
+}
+
+private extension CardView {
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
+
+        if abs(width) <= abs(screenCutoff) {
+            xOffset = 0
+            degrees = 0
+        }
     }
 }
 
 extension CardView {
+    private var screenCutoff: CGFloat {
+        (UIScreen.main.bounds.width / 2) * 0.8
+    }
+
     private var cardWidth: CGFloat {
         UIScreen.main.bounds.width - 20
     }
