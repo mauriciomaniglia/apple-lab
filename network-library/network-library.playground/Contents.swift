39d0c0b8c6029dc26b945b257dc99092cd5aa033
mauriciomaniglia <manigliamauricio@gmail.com>
2025-08-10 14:40:11 -0300

    Create a simple Webservice class

--- network-library/network-library.playground/Contents.swift
+++ network-library/network-library.playground/Contents.swift
@@ -1,3 +1,4 @@
+import XCPlayground
 import Foundation
 
 struct Resource<A> {
@@ -5,8 +6,23 @@ struct Resource<A> {
     let parse: (Data) -> A?
 }
 
-let url = URL(string: "http://localhost:8000/episodes.json")!
+let url = URL(string: "https://raw.githubusercontent.com/mauriciomaniglia/apple-lab/refs/heads/main/network-library/episodes.json")!
 
 let episodesResource = Resource<Data>(url: url, parse: { data in
     return data
 })
+
+final class Webservice {
+    func load<A>(resource: Resource<A>, completion: @escaping (A?) -> ()) {
+        URLSession.shared.dataTask(with: resource.url) { data, _, _ in
+            let result = data.flatMap(resource.parse)
+            completion(result)
+        }.resume()
+    }
+}
+
+Webservice().load(resource: episodesResource) { data in
+    print(data)
+}
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
