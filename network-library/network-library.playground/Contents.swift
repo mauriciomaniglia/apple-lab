9a7fdb962ce47b6fa570434ba78913debc94d7d2
mauriciomaniglia <manigliamauricio@gmail.com>
2025-08-10 15:25:44 -0300

    Code improvements

--- network-library/network-library.playground/Contents.swift
+++ network-library/network-library.playground/Contents.swift
@@ -16,20 +16,31 @@ extension Episode {
     }
 }
 
+extension Episode {
+    static let all = Resource<[Episode]>(url: url, parseJSON: { json in
+        guard let dictionaries = json as? [JSONDictionary] else { return nil }
+        return dictionaries.compactMap(Episode.init)
+    })
+}
+
+typealias JSONDictionary = [String: Any]
+
 struct Resource<A> {
     let url: URL
     let parse: (Data) -> A?
 }
 
+extension Resource {
+    init(url: URL, parseJSON: @escaping (Any) -> A?) {
+        self.url = url
+        self.parse = { data in
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            return json.flatMap(parseJSON)
+        }
+    }
+}
+
 let url = URL(string: "https://raw.githubusercontent.com/mauriciomaniglia/apple-lab/refs/heads/main/network-library/episodes.json")!
-
-typealias JSONDictionary = [String: AnyObject]
-
-let episodesResource = Resource<[Episode]>(url: url, parse: { data in
-    let json = try? JSONSerialization.jsonObject(with: data, options: [])
-    guard let dictionaries = json as? [JSONDictionary] else { return nil }
-    return dictionaries.flatMap(Episode.init)
-})
 
 final class Webservice {
     func load<A>(resource: Resource<A>, completion: @escaping (A?) -> ()) {
@@ -40,7 +51,7 @@ final class Webservice {
     }
 }
 
-Webservice().load(resource: episodesResource) { data in
+Webservice().load(resource: Episode.all) { data in
     print(data)
 }
 
