ef36a40ed6f110c3da02c3f7c9c1cddfc65c8ce6
mauriciomaniglia <manigliamauricio@gmail.com>
2025-10-04 08:57:21 -0300

    Remove project SportsCarAPI

--- SportsCarAPI/Sources/App/routes.swift
+++
@@ -1,9 +0,0 @@
-import Vapor
-
-func routes(_ app: Application) throws {
-    app.get("health") { req -> HTTPStatus in
-        return .ok
-    }
-    let carController = SportsCarController()
-    try app.register(collection: carController)
-}
