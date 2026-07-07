006ce4142e4db40a3d7ef0f9d3a3e0e713f07103
mauriciomaniglia <manigliamauricio@gmail.com>
2026-07-07 20:46:21 -0300

    Setup InstagramClone project

---
+++ SwiftUI/InstagramClone/InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Mauricio Cesar on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
