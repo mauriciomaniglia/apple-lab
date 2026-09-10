b9d9dc04069cc914a2f8890179957366a442a1ef
mauriciomaniglia <manigliamauricio@gmail.com>
2026-09-09 21:19:59 -0300

    Remove Ghibli project

--- SwiftUI/Ghibli/Ghibli/Networking/APIError.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum APIError: LocalizedError {
-    case invalidURL
-    case invalidResponse
-    case decoding(Error)
-    case networkingError(Error)
-}
--- SwiftUI/Ghibli/Ghibli/Networking/Services/GhibliService.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-protocol GhibliService: Sendable {
-    func fetchFilms() async throws -> [Film]
-    func fetchPerson(from URLString: String) async throws -> Person
-}
